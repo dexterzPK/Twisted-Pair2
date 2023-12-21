f43345aecd994b271be3cc773f47670a203fea73
Umang <jackerumang@gmail.com>
2023-12-22 02:01:23 +0530

    final ganddd shreyandddd

--- responsive-sushi-website-main/assets/js/main.js
+++ responsive-sushi-website-main/assets/js/main.js
@@ -65,7 +65,34 @@ const scrollActive=()=>{
 }
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById("theeme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+
+  themeButton.addEventListener("click", () => {
+    document.body.classList.togggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+  });
+}
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr=ScrollReveal({
