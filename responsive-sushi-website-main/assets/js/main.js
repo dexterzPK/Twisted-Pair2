a8ef262f5d1eb255830b510a3443472d863c125d
Prajwal-H-Rao <prajwalhr130@gmail.com>
2023-12-21 23:26:43 +0530

    Completed About

--- responsive-sushi-website-main/assets/js/main.js
+++ responsive-sushi-website-main/assets/js/main.js
@@ -5,7 +5,11 @@
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader=()=>{
+    const header = document.getElementById('header');
+    this.scrollY >=50?header.classList.add('bg-header'):header.classList.remove('bg-header')
+}
+window.addEventListener('scroll',scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
