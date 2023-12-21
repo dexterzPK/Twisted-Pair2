3765e793d852be7c3dd8604c562089a96183e0d7
dexterzPK <dontwanttolive945@gmail.com>
2023-12-21 17:21:05 +0530

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
+
 function App() {
-    return <div>App</div>;
+  return <div>App</div>;
 }
 
 export default App;
