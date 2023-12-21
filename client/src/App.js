784cc4c4a53b7c85391e48983c12e84ed5a3121c
Umang <jackerumang@gmail.com>
2023-12-21 18:31:37 +0530

    12umang

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Category from "./components/Category/Category";
-import SingleProduct from "./components/SingleProduct/SingleProduct";
-import Newsletter from "./components/Footer/Newsletter/Newsletter";
-import AppContext from "./utils/context";
 
 function App() {
   return <div>App</div>;
 }
 
-export default App;
+export default App
+
+
+// demo by umang  
+hey 
