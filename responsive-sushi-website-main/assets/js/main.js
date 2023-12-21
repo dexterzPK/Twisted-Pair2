0dada9d432d0df22cc79398b2480471cda625516
dexterzPK <134509031+dexterzPK@users.noreply.github.com>
2023-12-22 00:31:23 +0530

    dark/light theme

--- responsive-sushi-website-main/assets/js/main.js
+++ responsive-sushi-website-main/assets/js/main.js
@@ -1,19 +1,41 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theeme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
-/*=============== DARK LIGHT THEME ===============*/ 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+
+  themeButton.addEventListener("click", () => {
+    document.body.classList.togggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+  });
+}
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
